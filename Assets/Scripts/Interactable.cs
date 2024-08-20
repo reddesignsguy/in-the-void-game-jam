04da1c299925ae8b34ad60769192e006f982abf1
Albany Patriawan <49921782+reddesignsguy@users.noreply.github.com>
2024-08-19 19:25:08 -0700

    interactable objects dont move when the level is reset

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -146,6 +146,7 @@ public class Interactable : MonoBehaviour
 
     public void ResetState()
     {
+        _rb.velocity *= 0;
         transform.position = _spawnPosition;
         _gravityDirection = GravityDirection.NONE;
     }
