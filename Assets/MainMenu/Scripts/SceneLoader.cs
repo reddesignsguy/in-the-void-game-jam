ceb5d664045cab2db899c4f337ba96738c19b1a6
Albany Patriawan <49921782+reddesignsguy@users.noreply.github.com>
2024-08-22 19:54:43 -0700

    Main menu w/ transitions

---
+++ Assets/MainMenu/Scripts/SceneLoader.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public string sceneName;
+    public float transitionTime = 1;
+    public Animator transition;
+
+    public void StartScene()
+    {
+        StartCoroutine(StartSceneWithTransition(sceneName));
+    }
+
+    // Just a wrapper that plays a fade transition whenever the scene changes
+    public IEnumerator StartSceneWithTransition(string gameScene)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(gameScene);
+    }
+
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
