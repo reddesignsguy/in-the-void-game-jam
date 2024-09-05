f81d0fbcac32c4257db514ebaeb641d26286bec3
Albany Patriawan <49921782+reddesignsguy@users.noreply.github.com>
2024-09-04 19:56:12 -0700

    polish

--- Assets/Camera/CameraController.cs
+++ Assets/Camera/CameraController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using EZCameraShake;
 
 public class CameraController : MonoBehaviour
 {
@@ -16,8 +17,23 @@ public class CameraController : MonoBehaviour
         offset = new Vector3(0,0,zOffset);
     }
 
+
+    private void Start()
+    {
+        CameraShaker.Instance.ShakeOnce(1f, 0.3f, .1f, 3f);
+    }
+
+    private void Update()
+    {
+        if (Time.fixedTime > 5 && Time.fixedTime < 6)
+        {
+            CameraShaker.Instance.ShakeOnce(1f, 0.7f, .1f, 3f);
+        }
+    }
+
     void FixedUpdate()
     {
         transform.position = player.transform.position + offset;
+        
     }
 }
